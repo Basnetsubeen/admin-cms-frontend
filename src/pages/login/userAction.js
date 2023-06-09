e9f315cf0337e88739543243ce167209312fc5ca
subin basnet <Basnetsubeen>
2023-06-09 10:44:42 +1000

    done authenticate part

--- src/pages/login/userAction.js
+++ src/pages/login/userAction.js
@@ -6,7 +6,11 @@ import { setAdminUser } from "./userSlice";
 export const loginAdminUserAction = (data) => async (dispatch) => {
   const resultPromise = loginAdminUser(data);
   toast.promise(resultPromise, { pending: "Please wait..." });
-  const { status, message, user } = await resultPromise;
+  const { status, message, user, accessJWT, refreshJWT } = await resultPromise;
   toast[status](message);
-  status === "success" && dispatch(setAdminUser(user));
+  if (status === "success") {
+    sessionStorage.setItem("accessJWT", accessJWT);
+    localStorage.setItem("refreshJWT", refreshJWT);
+    dispatch(setAdminUser(user));
+  }
 };
